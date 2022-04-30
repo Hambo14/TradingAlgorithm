6a5bc7d5f3d47e818abd7e4b6671d68cdee5a5e5
Hambo14 <zanehambo14@gmail.com>
2022-05-01 01:24:38 +1000

    Added more functions

--- Algorithm/dataCollection.py
+++ Algorithm/dataCollection.py
@@ -3,30 +3,41 @@
 # Author: Zane Hampton
 #
 # Created: 26/04/2022
-# Last Edit: 26/04/2022
+# Last Edit: 01/05/2022
 #
 # Version: 0.01.00
 #
-# Notes: Functions to pull all data needed for the 
-#        trading algorithm
+# Notes: Functions to pull all data needed for the trading algorithm
 #
-# Script Name: parentClass.py 
+# Script Name: dataCollection.py 
 #
 # Description:
 #
-# Data is sourced using the IEX Cloud API. In order 
-# for the data to be successfully requested, you need
-# to have a valid IEX Cloud Token saved into your 
-# environment variables as IEX_TOKEN.
+# Historical price data is sourced using the IEX Cloud API. In order 
+# for the data to be successfully requested, you need to have a
+# valid IEX Cloud Token saved into your environment variables as 
+# IEX_TOKEN. The fundamental ratios used in the algorithm is sourced
+# using yfinance.
 #
 #---------------------------------------------------
 
 import pandas as pd
-import numpy as np
 import os
 import requests
+import yfinance as yf
 
-class iexCloud:
+def convertToString(stockTickers):
+    if type(stockTickers) == str:
+            stockTickers = stockTickers.split()
+
+    tickerString = ','.join(stockTickers)
+    
+    return tickerString, stockTickers
+
+class requestError(Exception):
+    pass
+
+class requestData:
 
     def __init__(self, sandbox: bool) -> None:
 
@@ -40,32 +51,81 @@ class iexCloud:
         self.params = {'token':self.token}
         pass
 
+    # Downloads monthly historical adjusted closing prices from the last quarter
     def historicalData(self, stockTickers, range = None, date = None):
-       endpoint = f'{self.base_url}/stock/{stockTickers}/chart'
-       if range:
-           endpoint += f'/{range}'
-       elif date:
-           endpoint += f'/date/{date}'
         
-       resp = requests.get(endpoint, params = self.params)
-       try:
-           historicalData = resp.json()
-       except:
-           return print(resp.raise_for_status())
-       return historicalData
+        tickerString, tickerList = convertToString(stockTickers = stockTickers)
+
+        if len(tickerList) == 1:
+            endpoint = f'{self.base_url}/stock/{tickerString}/chart'
+            if range:
+                endpoint += f'/{range}'
+            elif date:
+                endpoint += f'/date/{date}'
+        elif len(tickerList) > 1:
+            endpoint = f'{self.base_url}/stock/market/batch?symbols={tickerString}&types=chart'
+            if range:
+                endpoint += f'&range={range}'
+        elif len(tickerList) == 0:
+            raise ValueError("Add one or more stocks to the list")
+
+        endpoint += '&chartCloseOnly=true'
+        resp = requests.get(endpoint, params = self.params)
+        try:
+            historicalData = resp.json()
+        except:
+            raise requestError(f'The request failed. Check if URL is valid. API status: {resp.raise_for_status()}')
+
+        return historicalData
+
+    # Downloads the price to book and return on equity ratio. Returns a dataframe
+    def fundamentals(self, stockTickers: list or str):
+        
+        tickerList = convertToString(stockTickers)[1]
+        tickers = [yf.Ticker(ticker) for ticker in tickerList]
+
+        columns = ['Ticker','PB', 'ROE']
+        fundamentalsDf = pd.DataFrame(index=stockTickers)
+        for ticker in tickers:
+            tickerInfo = ticker.info
+
+            fundamentalsDf = fundamentalsDf.append(
+                pd.Series(
+                    [
+                        ticker.ticker,
+                        tickerInfo['priceToBook'],
+                        tickerInfo['returnOnEquity']
+                    ],
+                    index = columns
+                ),
+                ignore_index=True
+            )
+        fundamentalsDf.dropna(inplace = True)
+        return fundamentalsDf
+
+    # Finds the average monthly return for each stock
+    def averageMonthlyReturn(self, stockTickers: list):
+        monthlyData = self.historicalData(stockTickers, range = '3m')
+
+        monthlyReturnDict = {}
+        for ticker in stockTickers:
+            tickerDf = pd.json_normalize(monthlyData[f'{ticker}'], record_path=['chart'])
+            monthlyReturnDict[f'{ticker}'] = (tickerDf['changePercent'].mean())*21
+        return monthlyReturnDict
+    
+    # Compiles the price to book, return on equity and monthly rate of return into
+    # one dataframe
+    def completeData(self, stockTickers: list):
+        fundamentalsDf = self.fundamentals(stockTickers)
+        monthlyRoR = self.averageMonthlyReturn(stockTickers)
+        
+        fundamentalsDf.insert(3,'RoR',monthlyRoR.values(),True)
+        return fundamentalsDf
 
 if __name__ == '__main__':
-    base_url = 'https://cloud.iexapis.com/v1'
-    sandbox_url = 'https://sandbox.iexapis.com/stable/'
-
-    token = os.environ.get('IEX_TOKEN')
-    sandbox_token = os.environ.get('IEX_SANDBOX_TOKEN')
-
-    params = {'token':token}
-    sandbox_params = {'token':sandbox_token}
-
-    resp = requests.get(sandbox_url + 'stock/AAPL/chart', params = sandbox_params)
-    resp.raise_for_status()
-
-    df = pd.DataFrame(resp.json())
-    print(df.head())
+    getData = requestData(sandbox = True)
+    stockList = ['TWTR', 'GOOG', 'WFC']
+    
+    testDataFrame = getData.completeData(stockList)
+    print(testDataFrame)
+    None
